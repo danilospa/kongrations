6b5f69c96af13f7e38b7bc7de7428b16e9019b00
Danilo Albuquerque <danilospalbuquerque@gmail.com>
2018-04-03 00:37:03 -0300

    Enable the utilization of env variable on YAML

--- lib/kongrations/current_environment.rb
+++ lib/kongrations/current_environment.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'yaml'
+require 'erb'
 
 module Kongrations
   module CurrentEnvironment
@@ -8,6 +9,10 @@ module Kongrations
 
     def self.load!(name)
       yaml = File.read(FILE_NAME)
+
+      template = ERB.new(yaml)
+      yaml = template.result(binding)
+
       config = YAML.safe_load(yaml)
       environment = config['environments'].detect { |e| e['name'] == name }
 
