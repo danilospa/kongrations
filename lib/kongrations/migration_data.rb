7c8c6f96eb1f4394b6b7bee3a47034f0aa6360d9
Danilo Albuquerque <danilospalbuquerque@gmail.com>
2018-04-06 01:27:24 -0300

    Create migrations-data folder if it does not exist

--- lib/kongrations/migration_data.rb
+++ lib/kongrations/migration_data.rb
@@ -7,6 +7,8 @@ module Kongrations
   using HashExt
 
   module MigrationData
+    PATH = './migrations-data'
+
     def self.load!
       @data = File.exist?(file_name) ? JSON.parse(File.read(file_name)) : {}
     end
@@ -18,7 +20,7 @@ module Kongrations
     def self.save(migration_name, data)
       @data['last_migration'] = migration_name
       @data.deep_merge!(data) unless data.nil?
-      File.open(file_name, 'w') { |f| f.puts @data.to_json }
+      save_file
     end
 
     def self.data
@@ -26,7 +28,14 @@ module Kongrations
     end
 
     def self.file_name
-      "./migrations-data/#{CurrentEnvironment.name}.json"
+      "#{PATH}/#{CurrentEnvironment.name}.json"
     end
+
+    def self.save_file
+      Dir.mkdir(PATH) unless File.exist?(PATH)
+      File.open(file_name, 'w') { |f| f.puts @data.to_json }
+    end
+
+    private_class_method :save_file
   end
 end
